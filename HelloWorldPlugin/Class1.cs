66b1c5c0c2e84c3ff489d1894ea30b0d440349dc
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2015-01-05 23:31:54 +0200

    Hello world plugin

---
+++ HelloWorldPlugin/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HelloWorldPlugin
+{
+
+    public class HelloWorldPlugin : CKAN.IGUIPlugin
+    {
+
+        private readonly string VERSION = "v1.0.1";
+
+        public override void Initialize()
+        {
+            CKAN.Main.Instance.m_User.RaiseError("Hello World!");
+        }
+
+        public override void Deinitialize()
+        {
+            
+        }
+
+        public override string GetName()
+        {
+            return "Hello World- plugin";
+        }
+
+        public override CKAN.Version GetVersion()
+        {
+            return new CKAN.Version(VERSION);
+        }
+
+    }
+
+}
